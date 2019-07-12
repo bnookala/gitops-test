1566057d271907e4d204b99f78837e9a125715cc
Bhargav Nookala <nooknb@gmail.com>
2019-07-12 16:59:44 -0700

    Modifying test, adding component

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-git clone git@github.com:bnookala/hello-rings.git
+wget "https://github.com/Microsoft/fabrikate/releases/download/0.15.0/fab-v0.15.0-linux-amd64.zip"
+
+unzip fab-v0.15.0-linux-amd64.zip -d fab
+
+export PATH=$PATH:$HOME/fab
+
+fab install
+
+# git clone git@github.com:bnookala/hello-rings.git
+
