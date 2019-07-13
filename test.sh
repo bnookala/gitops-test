6095d064d70ee8a9b246fa7935a35e0c174a2ddb
Bhargav Nookala <nooknb@gmail.com>
2019-07-12 18:01:07 -0700

    Try test fix

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget "https://github.com/Microsoft/fabrikate/releases/download/0.12.0/fab-v0.12.0-linux-amd64.zip"
+wget "https://github.com/evanlouie/fabrikate/releases/download/fix-private-repos/fab-vfix-private-1-linux-amd64.zip"
 
-unzip fab-v0.12.0-linux-amd64.zip -d fab
+unzip fab-vfix-private-1-linux-amd64.zip -d fab
 
 mv ./fab/fab $HOME
 
