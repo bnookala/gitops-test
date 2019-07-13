774e160468ac554bd57f8f77051c875a9ae4e14e
Bhargav Nookala <nooknb@gmail.com>
2019-07-12 17:45:26 -0700

    Try .12

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-wget "https://github.com/Microsoft/fabrikate/releases/download/0.15.0/fab-v0.15.0-linux-amd64.zip"
+wget "https://github.com/Microsoft/fabrikate/releases/download/0.12.0/fab-v0.12.0-linux-amd64.zip"
 
-unzip fab-v0.15.0-linux-amd64.zip -d fab
+unzip fab-v0.12.0-linux-amd64.zip -d fab
 
 mv ./fab/fab $HOME
 
-$HOME/fab install
+$HOME/fab install --verbose
 
 # git clone git@github.com:bnookala/hello-rings.git
 
